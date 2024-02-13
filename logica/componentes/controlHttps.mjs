1728ecd24c7c0eb85081ae4d036d55d34561afcd
krackdev <webmaster@casavitini.com>
2024-02-13 20:13:24 +0100

    comprobado funcionamiento del redirector para el nuevo dominio pero desactivado de momento

--- logica/componentes/controlHttps.mjs
+++ logica/componentes/controlHttps.mjs
@@ -1,8 +1,8 @@
 export default (entrada, salida, siguinte) => {
     const dominioDePeticion = entrada.headers.host
     console.log("host", dominioDePeticion)
-    if (dominioDePeticion === 'ddns.net') {
-        return salida.redirect(301, 'https://casavitini.com' + entrada.originalUrl);
+    if (dominioDePeticion === 'lripoll.ddns.net') {
+       // return salida.redirect(301, 'https://casavitini.com' + entrada.originalUrl);
     } else if (!entrada.secure && (dominioDePeticion !== "localhost")) {
         return salida.redirect(301,'https://' + entrada.hostname + ':443' + entrada.url);
     }
