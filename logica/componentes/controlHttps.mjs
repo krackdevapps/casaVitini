1340025aa4cfeeb215974f270188926c27e8c252
krackDev <webmaster@casavitini.com>
2024-03-27 19:27:29 +0100

    activada redirecion a casavitini.com

--- logica/componentes/controlHttps.mjs
+++ logica/componentes/controlHttps.mjs
@@ -1,9 +1,9 @@
 export default (entrada, salida, siguinte) => {
     const dominioDePeticion = entrada.headers.host
     if (dominioDePeticion === 'lripoll.ddns.net') {
-       // return salida.redirect(301, 'https://casavitini.com' + entrada.originalUrl);
+        return salida.redirect(301, 'https://casavitini.com' + entrada.originalUrl);
     } else if (!entrada.secure && (dominioDePeticion !== "localhost")) {
-        return salida.redirect(301,'https://' + entrada.hostname + ':443' + entrada.url);
+        return salida.redirect(301, 'https://' + entrada.hostname + ':443' + entrada.url);
     }
     siguinte();
 }
