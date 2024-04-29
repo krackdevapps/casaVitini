5b296ef75f950b7bfff55d24bb766c8d2a6e816e
krackdevapps <166123211+krackdevapps@users.noreply.github.com>
2024-04-29 19:52:40 +0200

    update name of irrevelant folder

--- campoPruebas/ralentizador.js
+++
@@ -1,7 +0,0 @@
-const test = async() => {
-const selecionoAlgo = document.querySelector("body")
-selecionoAlgo.remove()
-await new Promise(resolve => setTimeout(resolve, 2000));
-selecionoAlgo.style.background = "red"
-}
-test()
