7a683a46e5baf23aa8a72ed2de26f65dbae4a0c5
KrackDev <166123211+krackdevapps@users.noreply.github.com>
2025-02-13 15:39:24 +0100

    renames robots.txt

--- router/router.mjs
+++ router/router.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import { calendarios_compartidos } from './routes/calendarios_compartidos.mjs';
 import { puerto } from './routes/puerto.mjs';
 import { arranque } from './routes/arranque.mjs';
-import { robots } from './routes/robos.mjs';
+import { robots } from './routes/robots.mjs';
 
 export const router = express.Router();
 router.get('/calendarios_compartidos/:href(*)', calendarios_compartidos)
